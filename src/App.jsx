c5e617d45051d52bd8e9353abb4c742f791e8a96
raisulhamim <raisulhamim.dev@gmail.com>
2025-11-28 21:56:32 +0600

    listitem component done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Listitem from './components/Listitem'
+
+const App = () => {
+  return (
+    <div>
+    <Listitem className="" text="Home"/>
+   
+    </div>
+  )
+}
+
+export default App
